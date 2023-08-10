3cfe6c45db61a3d60094d774f5fed2a5c01e04ba
somalingck <136596008+somalingck@users.noreply.github.com>
2023-08-11 03:02:36 +0530

    committing

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
+// Main.java
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwObj = new RWOnly("Initial Name");
+
+        // Use setter to set a value
+        rwObj.setName("New Name");
+
+        // Use getter to print the value
+        System.out.println(rwObj.getName()); // Output: New Name
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public RWOnly(String name) {
+        this.name=name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String newName){
+        name=newName;
+    }
 }
